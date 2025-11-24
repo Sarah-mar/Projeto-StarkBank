49386a8387ae12725c18c870dff1a8162c169d3d
sarinha <sarah.mar.figueiredo@gmail.com>
2025-11-24 20:28:55 -0300

    commit inicial analise_custos.js

---
+++ AnalistaFinanceiro/analise_custos.js
@@ -0,0 +1,90 @@
+const ctx = document.getElementById("income-statement-chart");
+
+function getLast12Months() {
+    const monthNames = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+
+    const today = new Date();
+    const months = [];
+
+    for (let i = 11; i >= 0; i--) {
+        const date = new Date(today.getFullYear(), today.getMonth() - i, 1);
+        months.push(`${monthNames[date.getMonth()]}/${date.getFullYear()}`);
+    }
+
+    return months;
+}
+
+const labels = getLast12Months()
+
+new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: "Receitas",
+                data: [-20, 0, 30, 50, 45, -20, 70, 43, 12, 31, 31, 21],
+                backgroundColor: "#637282b9"
+            },
+            {
+                label: "Despesas",
+                data: [-100, 30, -90, 85, 140, 50, 140, 31, 31, 31, 21, 11],
+                backgroundColor: "rgba(247, 109, 153, 0.95)"
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                labels: {
+                    usePointStyle: true,
+                    pointStyle: 'circle',
+                    padding: 20,
+                    font: {
+                        size: 14
+                    }
+                },
+                position: "top"
+            },
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        const valor = context.raw;
+                        return context.dataset.label + ": " + valor.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL"
+                        });
+                    }
+                }
+            }
+        },
+        scales: {
+            x: {
+                stacked: false
+            },
+            y: {
+                stacked: false,
+                ticks: {
+                    callback: function (valor) {
+                        return valor.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL"
+                        });
+                    }
+                }
+            }
+        }
+    }
+});
+
+document.getElementById("income-statement-overlay-button").onclick = function () {
+    document.getElementById("income-statement-overlay").classList.remove("hidden-income-statement-overlay");
+};
+
+document.getElementById("income-statement-overlay").onclick = function () {
+    this.classList.add("hidden-income-statement-overlay");
+};
